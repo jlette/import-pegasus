e75cf98fb2762a553df0ef1660e18b725272ed3f
jlette <lettevoueto.jimmy@gmail.com>
2026-02-23 15:24:43 +0100

    feat(app): ajoute la configuration des routes et les contrôleurs de base

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,25 @@
 <?php
-// config/routes.php
 
+/**
+ * Fichier de configuration des routes.
+ * Ce fichier centralise toutes les correspondances entre les URL demandées par l'utilisateur
+ * et les contrôleurs chargés de les traiter. Cela permet d'avoir une vue d'ensemble de l'application.
+ */
+
+use App\Controller\Csv\CsvController;
+use App\Controller\Error\ErrorController;
+
+// Liste des routes valides de l'application
 const AVAILABLE_ROUTES = [
-    'home' => 'homeController.php',
-    'contact' => 'contactController.php',
-    'privacy_policy' => 'privacyPolicyController.php',
-];
 
-const DEFAULT_ROUTE = AVAILABLE_ROUTES['home'];
+    '/' => [
+        'controller' => CsvController::class,
+        'action' => 'index'
+    ],
+
+];
+// Route de repli utilisée si l'URL demandée ne correspond à aucune route existante (Erreur 404)
+const DEFAULT_ROUTE = [
+    'controller' => ErrorController::class,
+    'action' => 'notFound'
+];
